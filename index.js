0db487bb7f3c966ccf9964401a00ac375ec99784
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-01-16 15:10:35 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(){
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat (){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat (){
+    return cats.shift();
+}
+
+function appendCat(){
+    const cats2 = [...cats, "Broom"];
+    return cats2;
+}
+
+function prependCat(){
+    const cats3 = ["Arnold", ...cats];
+    return cats3;
+}
+
+function removeLastCat (){
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat (){
+    return cats.slice(1);
+}
